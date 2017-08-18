c41ec1cfd57e3fb7234bb2a06bb7e21fb5e3a387
Vyacheslav Mychkin <vsm@g-taxi.com>
2017-08-18 11:47:22 +0600

    added filter to api keys list

--- src/dashboard/ApiKeyInfo.js
+++ src/dashboard/ApiKeyInfo.js
@@ -3,13 +3,27 @@ const availablePairs = ['BTC-BCC', 'BTC-NEO', 'BTC-ETH'];
 
 class ApiKeyInfo extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {pairs: availablePairs};
+    this.filterList = this.filterList.bind(this);
+  }
+
+  filterList(e) {
+    const filter = e.target.value.toLowerCase();
+    const filtered = availablePairs.filter(pair => pair.toLowerCase().search(filter) !== -1);
+    this.setState({pairs: filtered});
+  }
+
+
   render() {
     return (
       <div>
         <h2>ApiKey Info</h2>
+        <input placeholder="Search" onChange={this.filterList} />
         {this.props.apiKey ? (
         <ul>
-          {availablePairs.map(p => {
+          {this.state.pairs.map(p => {
             const pairEnabled = this.props.apiKey.pairs.indexOf(p) !== -1;
             return (
           <li key={p}>
