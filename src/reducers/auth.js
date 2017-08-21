024f137c302975452edea3cbda3cdaeb6ebc3e16
Vyacheslav Mychkin <vsm@g-taxi.com>
2017-08-21 23:23:00 +0600

    added saving userId in store, added logout action handling

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,10 +1,14 @@
-import { LOGGED_IN } from '../actions/auth';
+import { LOGGED_IN, LOGGED_OUT } from '../actions/auth';
 
 export default function reducer(auth = {}, action) {
   switch(action.type) {
     case LOGGED_IN:
-      localStorage.setItem('reduxState', JSON.stringify({auth: {loggedIn: true}}));
-      return {loggedIn: true};
+      const state = {loggedIn: true, userId: action.data._id};
+      localStorage.setItem('reduxState', JSON.stringify({auth: state}));
+      return state;
+    case LOGGED_OUT:
+      localStorage.setItem('reduxState', JSON.stringify({auth: {loggedIn: false}}));
+      return {loggedIn: false}
     default:
       return auth;
   }
