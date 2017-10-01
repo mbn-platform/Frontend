70863601abb7b85b341cfc59b0446fa71ac7ff48
Vyacheslav Mychkin <vsm@g-taxi.com>
2017-10-01 21:39:24 +0600

    styled charts

--- src/dashboard/ContractsChart.js
+++ src/dashboard/ContractsChart.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const ContractsChart = () => (
-   <div className="table">
-    <div className="table_title_wrapper clearfix">
-      <div className="table_title">Contract’s profit</div>
-    </div>
-  </div>
-);
+class ContractsChart extends React.Component {
+  render() {
+    return (
+      <div className="table">
+        <div className="table_title_wrapper clearfix">
+          <div className="table_title center">Profit as trader</div>
+        </div>
+        <div className="chart_title_total">
+          <span className="chart_title_total_span">Total:</span> 1.456 btc ~ 24 865 usd
+        </div>
+        <div id="contracts_chart">
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ContractsChart;
--- src/dashboard/TradersChart.js
+++ src/dashboard/TradersChart.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const TradersChart = () => (
-  <div className="table">
-    <div className="table_title_wrapper clearfix">
-      <div className="table_title">Trader’s profit</div>
-    </div>
-  </div>
-);
+class TradersChart extends React.Component {
+
+  render() {
+    return (
+      <div className="table">
+        <div className="table_title_wrapper clearfix">
+          <div className="table_title center">Profit as trader</div>
+        </div>
+        <div className="chart_title_total">
+          <span className="chart_title_total_span">Total:</span> 1.456 btc ~ 24 865 usd
+        </div>
+        <div id="traders_chart" style={{width: '100%', height: 205}}>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default TradersChart;
