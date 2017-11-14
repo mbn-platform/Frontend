f8fa81566745df4c74290d732c7e4ce593a8eead
amiznikov <amiznikov@gmail.com>
2017-11-14 18:56:36 +0300

    porting profile-feedback to dashboard-feedback

--- src/dashboard/SelectedContractInfo.js
+++ src/dashboard/SelectedContractInfo.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ContractInfo from './ContractInfo';
 import ContractFeedback from './ContractFeedback';
+import ProfileFeedbacks from './ProfileFeedbacks';
 
 const SelectedContractInfo = ({ contract, onContractRate }) => {
   if(!contract) {
     return null;
   } else {
     if(contract.state === 'FINISHED') {
-      return (
-        <ContractFeedback
-          onContractRate={onContractRate}
-          contract={contract}
+      if(contract.feedbacks && contract.feedbacks.length > 1) {
+        return (<ProfileFeedbacks
+          comments={contract.feedbacks}
         />);
+      } else {
+        return (
+
+          <ContractFeedback
+            onContractRate={onContractRate}
+            contract={contract}
+          />);        
+      }
+
     } else {
       return (<ContractInfo contract={contract}/>);
     }
