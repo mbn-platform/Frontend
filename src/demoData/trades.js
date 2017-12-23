3377157024adada632c3318e0600adb7e9beae07
Vyacheslav Mychkin <vsm@g-taxi.com>
2017-12-23 20:26:25 +0600

    synchonized profiledata generation with demo

--- src/demoData/trades.js
+++ src/demoData/trades.js
@@ -1,8 +1,13 @@
 import { getRandom } from './util';
 function generateTrade(tx, count, dt) {
   const price = getRandomPrice();
+  let date = '';
+  if(count < 32) {
+    date = (new Date(dt.getTime() + count * 24 * 60 * 60 * 1000)).toISOString();    
+  } else {
+    date = (new Date(dt.getTime() +  (count - 31) * 60 * 60 * 1000 +  31 * 24 * 60 * 60 * 1000)).toISOString();    
+  }
 
-  const date = (new Date(dt.getTime() - count * 24 * 60 * 60 * 1000)).toISOString();
   const type = getRandomTradeType();
   const amount = getRandom(100, 20);
   const amountCurrency = getRandomCurrency();
@@ -10,22 +15,13 @@ function generateTrade(tx, count, dt) {
   return { price, date, type, amount, amountCurrency, total, tx };
 }
 
-function generateDate(lastYear) {
-  const date = (new Date(Date.now() - getRandom(Date.now() - 1300000000000)))
-  date.setDate((new Date()).getDay() + 3);
-  if(lastYear) {
-    date.setFullYear((new Date()).getFullYear())
-    date.setMonth((new Date()).getMonth())
-  }
-  return date;
-}
-
 function generateTradesBlock(n = 20, tx,count, lastYear) {
   tx = tx || 'https://ropsten.etherscan.io/tx/0xf003ee3bdbd7c278864c2d4317669918e03b3dea7a0f5947051ea30c46e7c6f9';
   const trades = [];
-  const date = generateDate(lastYear)
-  for(let i = 0; i < n; i++) {
-    trades.push(generateTrade(tx,i, date));
+  const dayPoint = lastYear ? 2 : 0
+  const dateStart = new Date(Date.now() - ((n - count) * 31 * 24* 60 * 60 * 1000))
+  for(let i = 0; i < 31 + dayPoint; i++) {
+    trades.push(generateTrade(tx,i + 1, dateStart));
   }
   return trades;
 }
@@ -48,9 +44,9 @@ function getRandomCurrency() {
 export function generateTrades(numberOfBlock) {
   numberOfBlock = numberOfBlock || getRandom(4, 2);
   const trades = [];
-  for(let i = 0; i < numberOfBlock; i++) {
+  for(let i = 0; i < 10; i++) {
 
-    trades.push(generateTradesBlock(getRandom(10, 4), null, i, numberOfBlock - 1 == i ? true : false));
+    trades.push(generateTradesBlock(10, null, i, 9 == i ? true : false));
   }
   return trades;
 }
