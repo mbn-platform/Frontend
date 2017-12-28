ef34d046506dc49b30fd4199205d2c5226458822
amiznikov <amiznikov@gmail.com>
2017-12-28 13:34:58 +0530

    fix maxloss && currentprofit

--- src/demoData/contracts.js
+++ src/demoData/contracts.js
@@ -8,7 +8,6 @@ export const generateContract = (keyId, state, contractor) => {
   const startBalance = Math.floor(Math.random() * 100 + 20);
   const fee = Math.floor(Math.random() * 20 + 10);
   const roi = Math.floor(Math.random() * 20 + 10);
-  const maxLoss = Math.floor(Math.random() * 20 + 10);
   const targetBalance = Math.floor(startBalance * (100 + roi) / 100);
   const minBalance = Math.floor(startBalance * (100 - fee) / 100);
   let currentBalance;
@@ -17,6 +16,7 @@ export const generateContract = (keyId, state, contractor) => {
   } else {
     currentBalance = Math.floor(Math.random() * (targetBalance - minBalance) - (startBalance - minBalance)) + startBalance;
   }
+  const maxLoss = Math.round(Math.abs(Math.random() * ((currentBalance / startBalance - 1) * 100)  + ((currentBalance / startBalance - 1) * 100)) + 10);
   const left = Math.floor(targetBalance - currentBalance);
   const expireDate = Date.now() + Math.random() * 100 * 86400000;
   const startDate = Date.now() - Math.random() * 20 * 86400000;
