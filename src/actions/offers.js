03bdc1f7b10a243e048f1183ae66933ad75a2bd1
Vyacheslav Mychkin <vyacheslav.mychkin@gmail.com>
2018-01-18 20:17:40 +0600

    added request payment

--- src/actions/offers.js
+++ src/actions/offers.js
@@ -65,9 +65,67 @@ export function sendOffer(offer) {
 }
 
 export function payOffer(offer) {
-  return {
-    type: PAY_OFFER,
-    offer
-  };
+  return dispatch => {
+    window.web3.version.getNetwork((err, code) => {
+      if(err) {
+        alert('web3 error: no network');
+      } else {
+        switch(code) {
+          case '3':
+            const contract = window.web3.eth.contract(ABI).at(ADDRESS);
+            window.web3.eth.getAccounts((err, accs) => {
+              if(err) {
+                alert('Metamask error: cannot get account');
+              } else {
+                const account = accs[0];
+                if(!account) {
+                  alert('unlock metamask');
+                  return;
+                }
+                const { duration, maxLoss, startBalance, targetBalance, amount, _id } = offer;
+                const investor = offer.fromUser[0].name;
+                const investorAddress = offer.fromUser[0].addr;
+                const trader = offer.toUser[0].name;
+                const traderAddress = offer.toUser[0].addr;
+                let currency;
+                switch(offer.currency) {
+                  case 'BTC':
+                    currency = 1;
+                    break;
+                  case 'USDT':
+                    currency = 0;
+                    break;
+                  default:
+                    alert(offer.currency + ' not supoorted for contract yet');
+                    return;
+                }
+                contract.makeInstance(
+                  duration,
+                  maxLoss,
+                  startBalance,
+                  targetBalance,
+                  amount,
+                  investor,
+                  investorAddress,
+                  trader,
+                  traderAddress,
+                  '0x' + _id,
+                  currency,{value: amount},  (err, tx) => {
+                    if(err) {
+                      return;
+                    } else {
+                      alert('You have sent transaction to pay this request. If transaction completes succesfully you will receive the contract. Check the transaction status in metamask or here: https://ropsten.etherscan.io/tx/' + tx + '\nDo not pay this request again, if the transaction has not completed yet');
+                    }
+                  });
+
+              }
+            });
+            break;
+          default:
+            alert('Mercatus is in test mode, please select Ropsten network in Metamask');
+        }
+      }
+    });
+  }
 }
 
--- src/eth/MercatusFactory.js
+++ src/eth/MercatusFactory.js
@@ -1,84 +1,85 @@
 export const ABI = [
   {
-    'constant': false,
-    'inputs': [
+    "constant": false,
+    "inputs": [
       {
-        'name': '_be',
-        'type': 'address'
+        "name": "_duration",
+        "type": "uint256"
       },
       {
-        'name': '_deadline',
-        'type': 'uint256'
+        "name": "_maxLoss",
+        "type": "uint256"
       },
       {
-        'name': '_maxLoss',
-        'type': 'uint256'
+        "name": "_startBallance",
+        "type": "uint256"
       },
       {
-        'name': '_startBallance',
-        'type': 'uint256'
+        "name": "_targetBallance",
+        "type": "uint256"
       },
       {
-        'name': '_targetBallance',
-        'type': 'uint256'
+        "name": "_amount",
+        "type": "uint256"
       },
       {
-        'name': '_amount',
-        'type': 'uint256'
+        "name": "_investor",
+        "type": "string"
       },
       {
-        'name': '_invester',
-        'type': 'string'
+        "name": "_investorAddress",
+        "type": "address"
       },
       {
-        'name': '_investerAddress',
-        'type': 'address'
+        "name": "_trader",
+        "type": "string"
       },
       {
-        'name': '_trader',
-        'type': 'string'
+        "name": "_traderAddress",
+        "type": "address"
       },
       {
-        'name': '_traderAddress',
-        'type': 'address'
+        "name": "offer",
+        "type": "uint256"
       },
       {
-        'name': 'offer',
-        'type': 'uint256'
+        "name": "_currency",
+        "type": "uint256"
       }
     ],
-    'name': 'makeInstance',
-    'outputs': [
-      {
-        'name': '',
-        'type': 'address'
-      }
-    ],
-    'payable': true,
-    'type': 'function'
+    "name": "makeInstance",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
   },
   {
-    'anonymous': false,
-    'inputs': [
+    "anonymous": false,
+    "inputs": [
       {
-        'indexed': true,
-        'name': 'from',
-        'type': 'address'
+        "indexed": true,
+        "name": "from",
+        "type": "address"
       },
       {
-        'indexed': true,
-        'name': 'instance',
-        'type': 'address'
+        "indexed": true,
+        "name": "instance",
+        "type": "address"
       },
       {
-        'indexed': false,
-        'name': 'offer',
-        'type': 'uint256'
+        "indexed": false,
+        "name": "start",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "offer",
+        "type": "uint256"
       }
     ],
-    'name': 'spawnInstance',
-    'type': 'event'
+    "name": "spawnInstance",
+    "type": "event"
   }
 ];
 
-export const ADDRESS = '0xd9505442710f02ead1e50bdbc33abd0eb38ea435';
+export const ADDRESS = '0x660b2cad9c4d81c97ddb3069987927283c9887b2';
