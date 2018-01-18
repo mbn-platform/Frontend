82b67a075feb499cbf8ff24cf7c288c42f0b46f0
Vyacheslav Mychkin <vyacheslav.mychkin@gmail.com>
2018-01-18 21:30:17 +0600

    correct change displaying in header status

--- src/terminal/HeaderStatus.js
+++ src/terminal/HeaderStatus.js
@@ -34,17 +34,17 @@ class HeaderStatus extends React.Component {
               <Rate
                 pair="BTC/USD"
                 val={this.state['USDT-BTC'].Last ? Math.floor(this.state['USDT-BTC'].Last) : ''}
-                change={this.state['USDT-BTC'].last}
+                change={this.state['USDT-BTC'].PrevDay}
               />
               <Rate
                 pair="ETH/USD"
                 val={this.state['USDT-ETH'].Last ? this.state['USDT-ETH'].Last.toFixed(2) : ''}
-                change={this.state['USDT-ETH'].last}
+                change={this.state['USDT-ETH'].PrevDay}
               />
               <Rate
                 pair="BTC/ETH"
                 val={this.state['BTC-ETH'].Last ? parseFloat(this.state['BTC-ETH'].Last.toFixed(4)) : ''}
-                change={this.state['BTC-ETH'].last}
+                change={this.state['BTC-ETH'].PrevDay}
               />
             </Col>
             <Balance
@@ -88,16 +88,19 @@ class HeaderStatus extends React.Component {
   }
 }
 
-const Rate = ({ pair, val, change }) => (
-  <Col sm="4" md="4" lg="4" xs="4" className="curses row h-100 align-items-center justify-content-between">
-    <Col xs="auto" className="curses-name">{pair}</Col>
-    <Col xs="auto" className="curses-val">{val}</Col>
-    <Col xs="auto" className={classNames('curses-change', change > 0 ? 'up' : 'down')}>
-      <span className={classNames('icon', 'icon-dir', change > 0 ? 'icon-up-dir' : 'icon-down-dir')}></span>
-      {change ? change.toFixed(2) + '%' : ''}
+const Rate = ({ pair, val, change }) => {
+  change = change ? (val / change * 100 - 100) : null;
+  return (
+    <Col sm="4" md="4" lg="4" xs="4" className="curses row h-100 align-items-center justify-content-between">
+      <Col xs="auto" className="curses-name">{pair}</Col>
+      <Col xs="auto" className="curses-val">{val}</Col>
+      <Col xs="auto" className={classNames('curses-change', change > 0 ? 'up' : 'down')}>
+        <span className={classNames('icon', 'icon-dir', change > 0 ? 'icon-up-dir' : 'icon-down-dir')}></span>
+        {change ? change.toFixed(2) + '%' : ''}
+      </Col>
     </Col>
-  </Col>
-);
+  );
+}
 
 const Balance = ({first, second}) => (
   <Col xs="4" sm="4" md="4" lg="3" xl="2" className="row justify-content-end align-items-center">
