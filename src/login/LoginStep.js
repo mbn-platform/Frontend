230b03bad992027d95cfcc78a9b9e62b964c1b45
Vyacheslav Mychkin <vyacheslav.mychkin@gmail.com>
2018-01-24 16:26:15 +0600

    updated login text

--- src/login/LoginStep.js
+++ src/login/LoginStep.js
@@ -8,7 +8,7 @@ const LoginStep = ({ onClick, hasActiveAccount }) => {
 const MetamaskAccountNotActive = () => (
   <div className="login_step_login">
     <div className="login_step_has_account">Thank you for installing Metamask!</div>
-    <div className="login_step_login_text">Please login into your Metamask account.</div>
+    <div className="login_step_login_text">Please login into your Metamask account or check demo version.</div>
   </div>
 );
 
