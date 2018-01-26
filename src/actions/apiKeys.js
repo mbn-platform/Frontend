610c403e4303f0c75513fbecceb2e60467b1afc7
Vyacheslav Mychkin <vyacheslav.mychkin@gmail.com>
2018-01-26 09:07:54 +0600

    fixed api key updating

--- src/actions/apiKeys.js
+++ src/actions/apiKeys.js
@@ -1,4 +1,4 @@
-import { apiPut } from '../generic/apiCall';
+import { apiPut, apiDelete, apiPost, ApiError } from '../generic/apiCall';
 import { LOGGED_OUT } from '../actions/auth';
 export const DELETE_API_KEY = 'DELETE_API_KEY';
 export const ADD_API_KEY = 'ADD_API_KEY';
@@ -50,68 +50,6 @@ export function addApiKey(key) {
   };
 }
 
-class ApiError extends Error {
-  constructor(code) {
-    super();
-    this.apiErrorCode = code;
-  }
-}
-
-const defaultPostParams = {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  credentials: 'same-origin',
-  method: 'post',
-};
-
-
-
-ApiError.FORBIDDEN = -103;
-
-function jsonRequest(request) {
-  return request.then(res => res.json())
-    .then(json => {
-      const error = json.error;
-      if(error) {
-        throw new ApiError(error);
-      } else {
-        return json;
-      }
-    });
-}
-
-
-export function apiPost(url, params, data) {
-  params = {...defaultPostParams, ...params, body: JSON.stringify(data)};
-  return jsonRequest(window.fetch(url, params));
-}
-
-export function apiDelete(url, params) {
-  params = {...defaultDeleteParams, ...params};
-  return jsonRequest(window.fetch(url, params));
-}
-
-const defaultDeleteParams = {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  credentials: 'same-origin',
-  method: 'delete',
-};
-
-const defaultGetParams = {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  credentials: 'same-origin',
-  method: 'get',
-};
-
-export function apiGet(url, params) {
-  params = {...defaultGetParams, ...params};
-  return jsonRequest(window.fetch(url, params));
-}
 export function updateApiKey(key) {
   return dispatch => {
     apiPut('/api/key/' + key._id, null, {currencies: key.currencies})
