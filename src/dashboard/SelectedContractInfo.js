08a4c836cb5fdc62b425391a5aa27c535074c9ab
Vyacheslav Mychkin <vyacheslav.mychkin@gmail.com>
2018-05-24 17:18:33 +0300

    Revert "hiding contract info block if no contract is selected"
    This reverts commit 12554d7b5ee4864eba2989820fcb294d1eb12d53.

--- src/dashboard/SelectedContractInfo.js
+++ src/dashboard/SelectedContractInfo.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ContractInfo from './ContractInfo';
+import ContractInfoEmpty from './ContractInfoEmpty';
 import ContractFeedback from './ContractFeedback';
 import ProfileFeedbacks from './ProfileFeedbacks';
+import { Desktop, Mobile } from '../generic/MediaQuery';
 import { CONTRACT_STATE_FINISHED, CONTRACT_STATE_HALTED } from '../constants';
 
 const SelectedContractInfo = ({ contract, onContractRate, time, userId }) => {
   if(!contract) {
-    return null;
+    let dateNow = Date.now();
+    let contractDefault = {}
+    return (
+      <Desktop>
+        <ContractInfoEmpty/>
+      </Desktop>
+      );
   } else {
     if(contract.state === CONTRACT_STATE_FINISHED || contract.state === CONTRACT_STATE_HALTED) {
       if(canLeaveFeedback(contract, userId)) {
