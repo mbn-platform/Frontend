79b284fccc83ee161fde1272f738991a52b07803
deadw00d <pure.pudge@gmail.com>
2018-05-28 16:59:29 +0700

    #182 added toggle fullscreen button to trading view in terminal
    (cherry picked from commit 419770a)

--- src/terminal/TradingView.js
+++ src/terminal/TradingView.js
@@ -3,12 +3,24 @@ import { Col, Row } from 'reactstrap';
 import { Desktop } from '../generic/MediaQuery';
 
 class TradingView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullScreen: false
+    };
+  }
+
   render() {
+    let classNames = 'price-chart chart';
+    if (this.state.fullScreen) {
+      classNames += ' fullscreen-chart';
+    }
     return (
-      <Col className="price-chart chart">
+      <Col className={classNames}>
         <div className="price-chart__top justify-content-between row col-12">
           <Desktop>
             <Row className="chart-controls align-items-center justify-content-between">
+              <div className="control-resize" onClick={()=>this.setState({fullScreen: !this.state.fullScreen})}></div>
             </Row>
           </Desktop>
         </div>
