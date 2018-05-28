e1ba56f211eca4f84e2e17bc85dfa5ac353e77fa
deadw00d <pure.pudge@gmail.com>
2018-05-28 21:37:03 +0700

    #182 fixed classnames and added escape key handler

--- src/terminal/TradingView.js
+++ src/terminal/TradingView.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Col, Row } from 'reactstrap';
 import { Desktop } from '../generic/MediaQuery';
+import classNames from 'classnames';
+import {ESCAPE_KEYCODE} from '../constants';
+
+
 
 class TradingView extends React.Component {
   constructor(props) {
@@ -8,15 +12,26 @@ class TradingView extends React.Component {
     this.state = {
       fullScreen: false
     };
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentWillMount(){
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(event) {
+    if (this.state.fullScreen && event.keyCode === ESCAPE_KEYCODE) {
+      this.setState({fullScreen: false});
+    }
   }
 
   render() {
-    let classNames = 'price-chart chart';
-    if (this.state.fullScreen) {
-      classNames += ' fullscreen-chart';
-    }
     return (
-      <Col className={classNames}>
+      <Col className={classNames('price-chart','chart', {'fullscreen-chart': this.state.fullScreen})}>
         <div className="price-chart__top justify-content-between row col-12">
           <Desktop>
             <Row className="chart-controls align-items-center justify-content-between">
