11152931661fa886b33cb9e32fcbd1d1ec7bbe90
ilya <ilya-mir25@mail.ru>
2018-07-23 14:57:56 +0300

    - Add text decoration

--- src/components/login/Competition.js
+++ src/components/login/Competition.js
@@ -7,7 +7,7 @@ const Competition = () => (
   <a href="https://membrana.io/competition/?utm_source=beta_membrana_mobile&utm_medium=banner&utm_campaign=banner_mobile" className="competition__container">
     <img src={CompetitionIcon} className="competition__icon"/>
     <div className="competition__title">
-      <div className="competition__subtitle">Get FREE $100 on</div>
+      <div className="competition__subtitle"><span className="competition__subtitle_underline">Get FREE</span> $100 on</div>
       <div className="competition__subtitle_yellow">
         <img src={binanceLogo} className="competition__binance_logo"/>
         Binance Exchange
