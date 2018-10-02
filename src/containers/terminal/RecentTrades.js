48949508b3798f0e3874f618a37285f6de4a0cde
ilya <ilya-mir25@mail.ru>
2018-10-02 20:44:43 +0300

    - Remove jquerry from recentTrades page

--- src/containers/terminal/RecentTrades.js
+++ src/containers/terminal/RecentTrades.js
@@ -1,29 +1,103 @@
 import React from 'react';
 import classNames from 'classnames';
+import ReactTable from '../../components/SelectableReactTable';
 import { formatFloat } from '../../generic/util';
 import { Desktop } from '../../generic/MediaQuery';
 import {sortData, onColumnSort, classNameForColumnHeader}  from '../../generic/terminalSortFunctions';
 import {BigNumber} from 'bignumber.js';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, injectIntl } from 'react-intl';
 
 class RecentTrades extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {history: [], sort: {}};
+    this.state = {history: [], sort: {}, tableHeight: 200};
     this.sortData = sortData.bind(this);
     this.onColumnSort = onColumnSort.bind(this);
     this.sortFunctions = {};
+    this.tableWrapper = React.createRef();
   }
 
+  componentDidMount() {
+    this.setState({tableHeight: this.tableWrapper.current.offsetHeight + 130});
+  }
+
+  getColumns = () => {
+    const [base, secondary] = this.props.market.split('-');
+    return [
+      {
+        Header: <div onClick={() => this.onColumnSort('Price')}
+          className="terminal__header-wrapper">
+          <FormattedMessage id="terminal.priceRecent"
+            defaultMessage="Price ({base})" values={{base}}/>
+          <span className={classNameForColumnHeader(this.state, 'Price')}/>
+        </div>,
+        minWidth: 110,
+        className: 'table_col_value upper table_bot_col_value',
+        Cell: row => {
+          const isSellOrder = row.original.type === 'SELL';
+          return (<div className={isSellOrder ? 'up' : 'down'}>
+            {BigNumber(row.original.price).toString(10)} 
+            <span className={classNames('icon', 'icon-dir',
+              isSellOrder ? 'icon-down-dir' : 'icon-up-dir')}/>
+          </div>);
+        }
+      }, {
+        Header:<div onClick={() => this.onColumnSort('Quantity')}
+          className="terminal__header-wrapper">
+          <FormattedMessage id="terminal.tradeSize"
+            defaultMessage="Trade Size ({secondary})" values={{secondary}}/>
+          <span className={classNameForColumnHeader(this.state, 'Quantity')}/>
+        </div>,
+        className: 'table_col_value upper table_bot_col_value',
+        Cell: row => {
+          const isSellOrder = row.original.type === 'SELL';
+          return (<div className={`terminal__sub-cell ${isSellOrder ? 'up' : 'down'}`}>
+            {formatFloat(row.original.amount)}
+          </div>);
+        },
+        minWidth: 130,
+        headerClassName: 'table_bot_header_value',
+      }, {
+        minWidth: 70,
+        Header: <div onClick={() => this.onColumnSort('TimeStamp')}
+          className="terminal__header-wrapper">
+          <FormattedMessage id="terminal.time"
+            defaultMessage="Time" />
+          <span className={classNameForColumnHeader(this.state, 'TimeStamp')}/>
+        </div>,
+        headerClassName: 'table_bot_header_value',
+        Cell: row => {
+          const isSellOrder = row.original.type === 'SELL';
+          return (
+            <div className={`terminal__sub-cell ${isSellOrder ? 'up' : 'down'}`}>
+              {new Date(row.original.dt).toLocaleTimeString()}
+            </div>
+          );
+        },
+        className: 'table_col_value table_bot_col_value',
+      },
+      {
+        Header: '',
+        minWidth: 24,
+        className: 'table_col_value upper table_bot_col_value',
+        Cell: row => {
+          const isSellOrder = row.original.type === 'SELL';
+          return (<div className={`terminal__sub-cell ${isSellOrder ? 'up' : 'down'}`}>
+            {isSellOrder ? 'S' : 'B'}
+          </div>);
+        },
+      }
+    ];
+  }
 
   render() {
+    const { tableHeight } = this.state;
     let sortedData = [];
     const history = this.props.history;
     if(history && history.length) {
       sortedData = this.sortData(history);
     }
-    const [base, secondary] = this.props.market.split('-');
     return (
       <div className="trades-table chart col-12 col-sm-6 col-md-12">
         <div className="chart__top justify-content-between row">
@@ -36,70 +110,17 @@ class RecentTrades extends React.Component {
             </div>
           </Desktop>
         </div>
-
-        <div className="trades-table-wrapper js-table-wrapper">
-          <table className="table">
-            <thead>
-              <tr>
-                <th onClick={() => this.onColumnSort('Price')}>
-                  <div><FormattedMessage id="terminal.priceRecent"
-                    defaultMessage="Price ({base})" values={{base}}/>
-                  <span className={classNameForColumnHeader(this.state, 'Price')}/></div>
-                </th>
-                <th onClick={() => this.onColumnSort('Quantity')}>
-                  <div>
-                    <FormattedMessage id="terminal.tradeSize"
-                      defaultMessage="Trade Size ({secondary})" values={{secondary}}/>
-                    <span className={classNameForColumnHeader(this.state, 'Quantity')}/></div>
-                </th>
-                <th  onClick={() => this.onColumnSort('TimeStamp')}>
-                  <div>
-                    <FormattedMessage id="terminal.time"
-                      defaultMessage="Time" />
-                    <span className={classNameForColumnHeader(this.state, 'TimeStamp')}/></div>
-                </th>
-                <th>
-
-                </th>
-              </tr>
-            </thead>
-            <tbody className="tbody">
-              {sortedData.map(order => (
-                <OrderHistoryRow
-                  key={order.id}
-                  price={order.price}
-                  size={order.amount}
-                  type={order.type}
-                  date={new Date(order.dt)}
-                />
-              ))}
-            </tbody>
-          </table>
+        <div ref={this.tableWrapper} className="trades-table-wrapper terminal__recent-table-wrapper">
+          <ReactTable
+            columns={this.getColumns()}
+            data={sortedData}
+            scrollBarHeight={tableHeight}
+          />
         </div>
       </div>
     );
   }
 }
 
-const OrderHistoryRow = ({type, date, price, size, isBTC}) => {
-  const isSellOrder = type === 'SELL';
-  return (
-    <tr className={isSellOrder ? 'up' : 'down'}>
-      <td>
-        {BigNumber(price).toString(10)} <span className={classNames('icon', 'icon-dir',
-          isSellOrder ? 'icon-down-dir' : 'icon-up-dir')}/>
-      </td>
-      <td>
-        {formatFloat(size)}
-      </td>
-      <td>
-        {date.toLocaleTimeString()}
-      </td>
-      <td>
-        {isSellOrder ? 'S' : 'B'}
-      </td>
-    </tr>
-  );
-};
 
-export default RecentTrades;
+export default injectIntl(RecentTrades);
