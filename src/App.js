02c66d30c412005e0fc2fbf6debb8985503ffee5
ilya <ilya-mir25@mail.ru>
2018-10-03 17:34:58 +0300

    - Remove useless jquery code

--- src/App.js
+++ src/App.js
@@ -19,8 +19,6 @@ window.uncustomize = function() {
   $(document).off('click');
 };
 window.customize = function() {
-  $('[data-toggle="tooltip"]').tooltip();
-  $('[data-toggle="popover"]').popover();
 
   /*
     Global: Sticky table headers
@@ -52,86 +50,9 @@ window.customize = function() {
     processScrollableTable($table);
   });
 
-
-
-  /*
-    Global: Dropdowns behavior
-    */
-
-
-
-  /*
-    Ratings: 'Rank legend' popover
-    */
-  $('[data-toggle="ratings-help-popover"]').popover({
-    trigger: 'hover',
-    container: $('.ratings-main'),
-    html: true,
-    animation: false,
-    template: `
-      <div class="popover help-popover">
-        <div class="arrow"></div>
-        <div class="popover-body"></div>
-      </div>`,
-    content: function() {
-      let total   = $(this).data('total');
-      let success = $(this).data('success');
-      let investors = $('.ratings-investors.active').length ? ' success-investors' : '';
-
-      return `
-        <div class="total row">
-          <div><span class="round"></span></div>
-          <div><span>${total}</span></div>
-        </div>
-        <div class="success row${investors}">
-          <div><span class="round"></span></div>
-          <div><span>${success}</span></div>
-        </div>`;
-    }
-  });
-
-
   /*
     Ratings: ROI chart period selectbox
     */
-
-
-  $('.icon-fullscreen').each(function() {
-    $(this).click(function() {
-      let element = $(this).parent().parent().parent().parent().parent().get(0);
-      if (element.requestFullscreen) {
-        element.requestFullscreen();
-      }
-      else if(element.msRequestFullscreen) {
-        element.msRequestFullscreen();
-      }
-      else if(element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-      }
-      else if(element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen();
-      }
-    });
-  });
-
-  function editGroupEnable() {
-    if($('#cmn-toggle-4').prop('checked')) {
-      $('.info-screen').addClass('edit-block');
-      $('.edit-btn').addClass('active');
-      $('.edit-btn').text('save changes');
-    } else {
-      $('.info-screen').removeClass('edit-block');
-      $('.edit-btn').removeClass('active');
-      $('.edit-btn').text('edit');
-    }
-    if($('.info-screen').hasClass('disable-block')) {
-      $('.duration-contract .form-control').prop('disabled',true);
-      $('#cmn-toggle-4').prop('disabled',true);
-
-    }
-  }
-  editGroupEnable();
-  $( '#cmn-toggle-4' ).on( 'click', editGroupEnable );
 
   var mobileScreenShowButton = ($group, $item, countItem, textMore, textLess,heightEle) => {
     let heightList = 0;
@@ -150,18 +71,6 @@ window.customize = function() {
   $(window).resize((e) => {
     if($(window).width() > 1020){
 
-      $('.currency-settings tbody').height($('.currency-settings .card-body').outerHeight() - $('.currency-settings thead th').outerHeight());
-
-      $('.currency-settings tbody').mCustomScrollbar({
-        scrollButtons:{
-          enable:false
-        },
-        setHeight: ($('.currency-settings .card-body').outerHeight() - $('.currency-settings thead th').outerHeight()),
-        mouseWheel:{ preventDefault: true },
-        scrollbarPosition: 'inside',
-        autoExpandScrollbar:true,
-        theme: 'dark'
-      });
 
       $('.feedback-card .card-body .list-group').height($('.feedback-card .card-body').outerHeight() - 50);
       $('.feedback-card .card-body .list-group').mCustomScrollbar({
@@ -183,40 +92,8 @@ window.customize = function() {
       });
     }else{
       //$(selector).mCustomScrollbar("destroy"); //destroy scrollbar
-      $('.currency-settings tbody').mCustomScrollbar('destroy');
-
       $('.feedback-card .card-body .list-group').mCustomScrollbar('destroy');
-
-      /*let heightList = 0;
-      $(".feedback-card .card-body .list-group .list-group-item").each(function(i,el) {
-        if(i == 5) {
-          return false
-        } else {
-          heightList += $(this).height()
-        }
-      })
-      if($(".feedback-card .card-body .list-group .list-group-item").length > 5) {
-
-    //$(".feedback-card .card-body .list-group").height(heightList)
-        let countClickedMore = 0;
-        $(".feedback-card .card-body .list-group").readmore({
-          collapsedHeight:heightList,
-          speed: 500,
-          moreLink: '<div class="d-flex justify-content-center show-feedbacks-block"><button type="button" class="show-feedbacks-btn btn btn-secondary">show next 5 feedbacks</button></div>',
-          lessLink: '<div class="d-flex justify-content-center show-feedbacks-block"><button type="button" class="show-feedbacks-btn btn btn-secondary">show less 5 feedbacks</button></div>',
-          afterToggle: function(trigger, element, expanded) {
-            if(! expanded) {
-    //$(".feedback-card .card-body .list-group").readmore('toggle');
-            }
-          }
-        });
-      } else {
-        $(".feedback-card .card-body .list-group").height(heightList)
-      }*/
       mobileScreenShowButton($('.feedback-card .card-body .list-group'),$('.feedback-card .card-body .list-group .list-group-item'), 5,'show next 5 feedbacks','show less 5 feedbacks');
-    //mobileScreenShowButton($(".currency-settings table tbody"),$(".currency-settings tbody tr") ,10 ,'show next 10 currencies' ,'show previous 10 currencies',$(".currency-settings tbody tr:not(.empty-tr) td").outerHeight())
-
-
 
     }
   }).trigger('resize');
