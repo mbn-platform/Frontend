a081acb257b8793b1aefa02e5e3ea91963d09b83
ilya <ilya-mir25@mail.ru>
2018-10-08 13:08:30 +0300

    Remove useless feature

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -6,60 +6,62 @@ import { FormattedMessage } from 'react-intl';
 
 const Stats = ({ traderRating, investorRating, roiInUSD, roiInBTC, totalInBTC, totalInUSDT })  => (
   <Row className="justify-content-between raiting-block">
-    <Col xs="auto" className="raiting-left-item">
-      <div className="content-fuild">
-        <div className="row-fuild raiting">
-          <Col xs="auto">
-            <div className="description-text">
-              <FormattedMessage id="rankInRating"
-                defaultMessage="Rank in traders rating:"/>
-            </div>
-            <div className="value-text">
-              <span className="number-value-text">#</span><span className="text-underline">{}<hr/></span>
-            </div>
-          </Col>
-        </div>
-        <div className="row-fuild money">
-          <Col>
-            <div className="description-text">
-              <FormattedMessage id="returnOnInvestment"
-                defaultMessage="return on investment (ROI):"/>
-            </div>
-            <div className="value-text">
-              {roiInUSD}<span className="currency-value-usd-text letter-spacing-1">
-                <FormattedMessage id="percentInUsd"
-                  defaultMessage="% in usd"/>
-              </span>
-              <span className="icon icon-help icon-help-web-button"  id="help-icon-roi"/>
-              <UncontrolledTooltip target="help-icon-roi">
-                <FormattedMessage id="roiCalculatedBy"
-                  defaultMessage="ROI CALCULATED BY MEMBRANA ANALYSIS SYSTEM"/>
-              </UncontrolledTooltip>
+    {/*TODO Add correct logic*/}
+    {/*<Col xs="auto" className="raiting-left-item">*/}
+    {/*<div className="content-fuild">*/}
+    {/*<div className="row-fuild raiting">*/}
+    {/*<Col xs="auto">*/}
+    {/*<div className="description-text">*/}
+    {/*<FormattedMessage id="rankInRating"*/}
+    {/*defaultMessage="Rank in traders rating:"/>*/}
+    {/*</div>*/}
+    {/*<div className="value-text">*/}
+    {/*<span className="number-value-text">#</span><span className="text-underline">{}<hr/></span>*/}
+    {/*</div>*/}
+    {/*</Col>*/}
+    {/*</div>*/}
+    {/*<div className="row-fuild money">*/}
+    {/*<Col>*/}
+    {/*<div className="description-text">*/}
+    {/*<FormattedMessage id="returnOnInvestment"*/}
+    {/*defaultMessage="return on investment (ROI):"/>*/}
+    {/*</div>*/}
+    {/*<div className="value-text">*/}
+    {/*{roiInUSD}<span className="currency-value-usd-text letter-spacing-1">*/}
+    {/*<FormattedMessage id="percentInUsd"*/}
+    {/*defaultMessage="% in usd"/>*/}
+    {/*</span>*/}
+    {/*<span className="icon icon-help icon-help-web-button"  id="help-icon-roi"/>*/}
+    {/*<UncontrolledTooltip target="help-icon-roi">*/}
+    {/*<FormattedMessage id="roiCalculatedBy"*/}
+    {/*defaultMessage="ROI CALCULATED BY MEMBRANA ANALYSIS SYSTEM"/>*/}
+    {/*</UncontrolledTooltip>*/}
 
-            </div>
-            <div className="description-text btc-text">
-              ~ {roiInBTC} <span className="currency-value-btc-text letter-spacing-1">
-                <FormattedMessage id="percentInBtc"
-                  defaultMessage="% in btc"/>
-              </span>
-            </div>            
-          </Col>
-        </div>
-      </div>
-    </Col>
+    {/*</div>*/}
+    {/*<div className="description-text btc-text">*/}
+    {/*~ {roiInBTC} <span className="currency-value-btc-text letter-spacing-1">*/}
+    {/*<FormattedMessage id="percentInBtc"*/}
+    {/*defaultMessage="% in btc"/>*/}
+    {/*</span>*/}
+    {/*</div>            */}
+    {/*</Col>*/}
+    {/*</div>*/}
+    {/*</div>*/}
+    {/*</Col>*/}
     <Col className="col-auto raiting-right-item">
       <div className="container-fuild">
-        <div className="row-fuild raiting">
-          <Col xs="auto">
-            <div className="description-text">
-              <FormattedMessage id="rankInInvestorsRating"
-                defaultMessage="Rank in investors rating:"/>
-            </div>
-            <div className="value-text green">
-              <span className="number-value-text">#</span><span className="text-underline">{}<hr/></span>
-            </div>
-          </Col>
-        </div>
+        {/*TODO Add correct logic*/}
+        {/*<div className="row-fuild raiting">*/}
+        {/*<Col xs="auto">*/}
+        {/*<div className="description-text">*/}
+        {/*<FormattedMessage id="rankInInvestorsRating"*/}
+        {/*defaultMessage="Rank in investors rating:"/>*/}
+        {/*</div>*/}
+        {/*<div className="value-text green">*/}
+        {/*<span className="number-value-text">#</span><span className="text-underline">{}<hr/></span>*/}
+        {/*</div>*/}
+        {/*</Col>*/}
+        {/*</div>*/}
         <div className="row-fuild money">
           <div className="col-auto">
             <div className="description-text">
