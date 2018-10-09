6fcd0282d70789feac7c30f2f85f8f3b99bb31cb
ilya <ilya-mir25@mail.ru>
2018-10-09 15:51:15 +0300

    - Remove render stats to function

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,64 +4,80 @@ import { Row, Col } from 'reactstrap';
 import { UncontrolledTooltip } from 'reactstrap';
 import { FormattedMessage } from 'react-intl';
 
+/* eslint no-unused-vars: 0 */
+
+
+const renderInvestorsRank = () => {
+  return (
+    <div className="row-fuild raiting">
+      <Col xs="auto">
+        <div className="description-text">
+          <FormattedMessage id="rankInInvestorsRating"
+            defaultMessage="Rank in investors rating:"/>
+        </div>
+        <div className="value-text green">
+          <span className="number-value-text">#</span><span className="text-underline">{}<hr/></span>
+        </div>
+      </Col>
+    </div>);
+};
+
+const renderRankInTrades = () => {
+  return (
+    <div className="row-fuild raiting">
+      <Col xs="auto">
+        <div className="description-text">
+          <FormattedMessage id="rankInRating"
+            defaultMessage="Rank in traders rating:"/>
+        </div>
+        <div className="value-text">
+          <span className="number-value-text">#</span><span className="text-underline">{}<hr/></span>
+        </div>
+      </Col>
+    </div>
+  );
+};
+
+const renderRoi = (roiInUSD, roiInBTC ) => (
+  <div className="row-fuild money">
+    <Col>
+      <div className="description-text">
+        <FormattedMessage id="returnOnInvestment"
+          defaultMessage="return on investment (ROI):"/>
+      </div>
+      <div className="value-text">
+        {roiInUSD}<span className="currency-value-usd-text letter-spacing-1">
+          <FormattedMessage id="percentInUsd"
+            defaultMessage="% in usd"/>
+        </span>
+        <span className="icon icon-help icon-help-web-button"  id="help-icon-roi"/>
+        <UncontrolledTooltip target="help-icon-roi">
+          <FormattedMessage id="roiCalculatedBy"
+            defaultMessage="ROI CALCULATED BY MEMBRANA ANALYSIS SYSTEM"/>
+        </UncontrolledTooltip>
+
+      </div>
+      <div className="description-text btc-text">
+        ~ {roiInBTC} <span className="currency-value-btc-text letter-spacing-1">
+          <FormattedMessage id="percentInBtc"
+            defaultMessage="% in btc"/>
+        </span>
+      </div>
+    </Col>
+  </div>
+);
+
 const Stats = ({ traderRating, investorRating, roiInUSD, roiInBTC, totalInBTC, totalInUSDT })  => (
   <Row className="justify-content-between raiting-block">
-    {/*TODO Add correct logic*/}
     {/*<Col xs="auto" className="raiting-left-item">*/}
     {/*<div className="content-fuild">*/}
-    {/*<div className="row-fuild raiting">*/}
-    {/*<Col xs="auto">*/}
-    {/*<div className="description-text">*/}
-    {/*<FormattedMessage id="rankInRating"*/}
-    {/*defaultMessage="Rank in traders rating:"/>*/}
-    {/*</div>*/}
-    {/*<div className="value-text">*/}
-    {/*<span className="number-value-text">#</span><span className="text-underline">{}<hr/></span>*/}
-    {/*</div>*/}
-    {/*</Col>*/}
-    {/*</div>*/}
-    {/*<div className="row-fuild money">*/}
-    {/*<Col>*/}
-    {/*<div className="description-text">*/}
-    {/*<FormattedMessage id="returnOnInvestment"*/}
-    {/*defaultMessage="return on investment (ROI):"/>*/}
-    {/*</div>*/}
-    {/*<div className="value-text">*/}
-    {/*{roiInUSD}<span className="currency-value-usd-text letter-spacing-1">*/}
-    {/*<FormattedMessage id="percentInUsd"*/}
-    {/*defaultMessage="% in usd"/>*/}
-    {/*</span>*/}
-    {/*<span className="icon icon-help icon-help-web-button"  id="help-icon-roi"/>*/}
-    {/*<UncontrolledTooltip target="help-icon-roi">*/}
-    {/*<FormattedMessage id="roiCalculatedBy"*/}
-    {/*defaultMessage="ROI CALCULATED BY MEMBRANA ANALYSIS SYSTEM"/>*/}
-    {/*</UncontrolledTooltip>*/}
-
-    {/*</div>*/}
-    {/*<div className="description-text btc-text">*/}
-    {/*~ {roiInBTC} <span className="currency-value-btc-text letter-spacing-1">*/}
-    {/*<FormattedMessage id="percentInBtc"*/}
-    {/*defaultMessage="% in btc"/>*/}
-    {/*</span>*/}
-    {/*</div>            */}
-    {/*</Col>*/}
-    {/*</div>*/}
+    {/*{renderRankInTrades()}*/}
+    {/*{renderROI(roiInUSD, roiInBTC)}*/}
     {/*</div>*/}
     {/*</Col>*/}
     <Col className="col-auto raiting-right-item">
       <div className="container-fuild">
-        {/*TODO Add correct logic*/}
-        {/*<div className="row-fuild raiting">*/}
-        {/*<Col xs="auto">*/}
-        {/*<div className="description-text">*/}
-        {/*<FormattedMessage id="rankInInvestorsRating"*/}
-        {/*defaultMessage="Rank in investors rating:"/>*/}
-        {/*</div>*/}
-        {/*<div className="value-text green">*/}
-        {/*<span className="number-value-text">#</span><span className="text-underline">{}<hr/></span>*/}
-        {/*</div>*/}
-        {/*</Col>*/}
-        {/*</div>*/}
+        {/*{renderInvestorsRank()}*/}
         <div className="row-fuild money">
           <div className="col-auto">
             <div className="description-text">
@@ -92,5 +108,7 @@ Stats.propTypes = {
   roiInBTC: PropTypes.number,
   roiInUSD: PropTypes.number,
 };
+
+//eslint no-unused-vars: 2
 
 export default Stats;
