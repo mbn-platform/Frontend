d680c6c5aafb8a6fb05fd4073dd1e5f54177d7f2
Vyacheslav Mychkin <vyacheslav.mychkin@gmail.com>
2018-12-09 10:33:14 +0300

    added banners for presale

--- src/containers/login/Competition.js
+++ src/containers/login/Competition.js
@@ -1,32 +1,52 @@
 import React from 'react';
 import './Competition.css';
-import binanceLogo from '../../assets/svg/binanceLogo.svg';
-import CompetitionIcon from '../../assets/img/Competition.png';
-import { FormattedMessage } from 'react-intl';
 
-const Competition = () => (
-  <a href="https://membrana.io/competition/?utm_source=beta_membrana_mobile&utm_medium=banner&utm_campaign=banner_mobile" className="competition__container">
-    <img src={CompetitionIcon} alt="Competition" className="competition__icon"/>
-    <div className="competition__title">
-      <div className="competition__subtitle">
-        <span className="competition__subtitle_underline">
-          <FormattedMessage
-            id="competition.free"
-            defaultMessage="Get FREE"
-          /></span>
-        <FormattedMessage
-          id="competition.prize"
-          defaultMessage=" $100 on"
-        />
-      </div>
-      <div className="competition__subtitle_yellow">
-        <img src={binanceLogo} alt="binance" className="competition__binance_logo"/>
-        <FormattedMessage
-          id="competition.binanceExchange"
-          defaultMessage="Binance Exchange"
-        />
-      </div>
-    </div>
-  </a>
-);
-export default Competition;
+const bannerTexts = [
+  'Get X% bonus on Membrana Platform presale',
+  'Want passive income? Join presale now with X% bonus',
+  'Make smart invesmtent with X% bonus, join presale now',
+];
+
+class Banner extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = getRandomBanner();
+  }
+
+  render() {
+    return (
+      <a href={this.state.ref} className="competition__container">
+        <div className="competition__subtitle competition__title">
+          {this.state.text}
+        </div>
+      </a>
+    );
+  }
+}
+export default Banner;
+
+function getRandomBanner() {
+  const percent = getBonusPercent();
+  const index = Math.floor(Math.random() * bannerTexts.length);
+  console.log(index);
+  const text = bannerTexts[index];
+  const ref = `https://membrana.io/presale/?utm_source=beta_membrana_mobile&utm_medium=banner&utm_campaign=banner_mobile&utm_content=${index}`;
+  const bannerText = text.replace('X%', `${percent}%`);
+  return { ref, text: bannerText };
+}
+
+function getBonusPercent() {
+  const date = Date.now();
+  if (date < 1544486400000) {
+    return 70;
+  } else if (date < 1545696000000) {
+    return 50;
+  } else if (date < 1546905600000) {
+    return 35;
+  } else if (date < 1548115200000) {
+    return 30;
+  } else {
+    return 25;
+  }
+}
