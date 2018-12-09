030342d34062fb8f8ca42219678c211e5448450d
Romanist <hello@h-w-h-p.digital>
2018-12-09 19:30:21 +0300

    banner changes

--- src/containers/login/Competition.js
+++ src/containers/login/Competition.js
@@ -12,14 +12,21 @@ class Banner extends React.Component {
 
   render() {
     return (
-      <a href={this.state.ref} className="competition__container">
+      <a id="popUp" href={this.state.ref} className={`competition__container competition__container-${this.state.index}`}>
         {this.state.banner}
         <img src={this.state.image} alt="Competition" className="competition__icon"/>
+        <span className="competition__closer" onClick={handleClick}></span>
       </a>
     );
   }
 }
 export default Banner;
+
+function handleClick(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  document.getElementById('popUp').classList.add('competition__container-hidden');
+}
 
 function getRandomBanner() {
   const index = Math.floor(Math.random() * bannerTexts.length);
@@ -27,11 +34,11 @@ function getRandomBanner() {
   const ref = `https://membrana.io/presale/?utm_source=beta_membrana_mobile&utm_medium=banner&utm_campaign=banner_mobile&utm_content=${index}`;
   let image;
   if (index === 2) {
-    image = Lambo;
-  } else {
     image = HappyTrader;
+  } else {
+    image = Lambo;
   }
-  return { ref, banner, image };
+  return { ref, banner, image, index };
 }
 
 const bannerTexts = [
@@ -42,7 +49,7 @@ const bannerTexts = [
     Want passive income? <u>Join presale now</u> with {getBonusPercent()}% bonus!
   </div>),
   (<div className="competition__subtitle competition__title">
-    Make smart invesmtent with {getBonusPercent()}% bonus. <u>Join presale now</u>!
+    Make <br /> smart invesmtent with {getBonusPercent()}% bonus. <u>Join presale now</u>!
   </div>),
 ];
 
