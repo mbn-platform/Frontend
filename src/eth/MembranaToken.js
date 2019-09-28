e6da060a3e6658ebfb1b4cc5969c8c8c53979433
Vyacheslav Mychkin <vyacheslav.mychkin@gmail.com>
2019-09-28 17:17:19 +0300

    added function to transfer mbn tokens

---
+++ src/eth/MembranaToken.js
@@ -0,0 +1,75 @@
+export const ABI = [
+  {
+    'constant': true,
+    'inputs': [],
+    'name': 'name',
+    'outputs': [
+      {
+        'name': '',
+        'type': 'string'
+      }
+    ],
+    'payable': false,
+    'stateMutability': 'view',
+    'type': 'function'
+  },
+  {
+    'constant': true,
+    'inputs': [
+      {
+        'name': 'owner',
+        'type': 'address'
+      }
+    ],
+    'name': 'balanceOf',
+    'outputs': [
+      {
+        'name': '',
+        'type': 'uint256'
+      }
+    ],
+    'payable': false,
+    'stateMutability': 'view',
+    'type': 'function'
+  },
+  {
+    'constant': false,
+    'inputs': [
+      {
+        'name': 'to',
+        'type': 'address'
+      },
+      {
+        'name': 'value',
+        'type': 'uint256'
+      }
+    ],
+    'name': 'transfer',
+    'outputs': [
+      {
+        'name': '',
+        'type': 'bool'
+      }
+    ],
+    'payable': false,
+    'stateMutability': 'nonpayable',
+    'type': 'function'
+  },
+];
+
+window.mbnTransfer = transfer;
+
+export const ADDRESS = process.env.REACT_APP_MBN_TOKEN_ADDRESS;
+
+export function transfer(web3, to, amount) {
+  return new Promise((res, rej) => {
+    const contract = web3.eth.contract(ABI).at(ADDRESS);
+    contract.transfer(to, amount, (err, tx) => {
+      if (err) {
+        rej(err);
+      } else {
+        res(tx);
+      }
+    });
+  });
+}
