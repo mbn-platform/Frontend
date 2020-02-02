dfe2a0b4d26ba651119626b8a859b4fbaed79437
Eugene Zhylenko <jenyajilenko@gmail.com>
2020-02-02 23:59:20 +0200

    refactor for apiKeys reducer

--- src/reducers/apiKeys.js
+++ src/reducers/apiKeys.js
@@ -1,3 +1,6 @@
+import { combineReducers } from 'redux';
+
+import { reducerCreator } from 'generic/util';
 import {
   DELETE_API_KEY,
   ADD_API_KEY,
@@ -6,72 +9,56 @@ import {
   UPDATE_BOT_KEYS,
   ADD_BOT_KEYS,
   DELETE_BOT_KEYS
-} from '../actions/apiKeys';
-import {UPDATE_DASHBOARD, UPDATE_KEYS} from '../actions/dashboard';
-import { combineReducers } from 'redux';
+} from 'actions/apiKeys';
+import {
+  UPDATE_DASHBOARD, UPDATE_KEYS,
+} from 'actions/dashboard';
 
-function ownKeys(keys = [], action) {
-  switch(action.type) {
-    case DELETE_API_KEY:
-      return keys.filter(k => k._id !== action.apiKey._id);
-    case UPDATE_API_KEY:
-      return keys.map(k => k._id === action.apiKey._id ?
-        action.apiKey : k);
-    case ADD_API_KEY:
-      const key = keys.find(key => key._id === action.apiKey._id);
-      if(!key) {
-        return keys.concat(action.apiKey);
-      } else {
-        return keys;
-      }
-    case UPDATE_KEYS:
-      return action.data;
-    case UPDATE_API_KEY_BALANCE: {
-      const key = keys.find(k => k._id === action._id);
-      if(!key) {
-        return keys;
-      }
-      const balances = action.balances;
-      const updatedBalances = key.balances.map(b => {
-        const updatedBalance = balances[b.name];
-        if(updatedBalance) {
-          return {...b, ...updatedBalance};
-        } else {
-          return b;
-        }
-      });
-      const newKey = {...key};
-      newKey.balances = key.balances.length > 0 ? updatedBalances : action.balances;
-      newKey.totalInBTC = action.totalInBTC;
-      newKey.totalInUSDT = action.totalInUSDT;
-      return keys.map(k => k._id === newKey._id ? newKey : k);
+const ownKeysReducerList = {
+  [DELETE_API_KEY]: (keys, { apiKey }) => {
+    return keys.filter(k => k._id !== apiKey._id);
+  },
+  [UPDATE_API_KEY]: (keys, { apiKey }) => {
+    return keys.map(k => k._id === apiKey._id ? apiKey : k);
+  },
+  [ADD_API_KEY]: (keys, { apiKey }) => {
+    const key = keys.find(key => key._id === apiKey._id);
+    return key ? keys : keys.concat(apiKey);
+  },
+  [UPDATE_KEYS]: (_, { data }) => { return data; },
+  [UPDATE_API_KEY_BALANCE]: (keys, { _id, balances, totalInBTC, totalInUSDT }) => {
+    const key = keys.find(k => k._id === _id);
+
+    if(!key) {
+      return keys;
     }
-    default:
-      return keys;
-  }
-}
+    const updatedBalances = key.balances.map(b => {
+      const updatedBalance = balances[b.name];
+      return updatedBalance ? { ...b, ...updatedBalance } : b;
+    });
+    const newKey = {...key};
+    newKey.balances = key.balances.length > 0 ? updatedBalances : balances;
+    newKey.totalInBTC = totalInBTC;
+    newKey.totalInUSDT = totalInUSDT;
 
-function botKeys(keys = [], {type, data, _id, keyID}) {
-  switch(type) {
-    case DELETE_BOT_KEYS:
-      return keys.filter(k => k._id !== keyID);
-    case UPDATE_BOT_KEYS:
-      return data;
-    case ADD_BOT_KEYS:
-      return [...keys, data];
-    default:
-      return keys;
-  }
-}
+    return keys.map(k => k._id === newKey._id ? newKey : k);
+  },
+};
 
-function receivedKeys(keys = [], action) {
-  switch(action.type) {
-    case UPDATE_DASHBOARD:
-      return action.data.keys.receivedKeys;
-    default:
-      return keys;
-  }
-}
+const botKeysReducerList = {
+  [DELETE_BOT_KEYS]: (keys, { keyID }) => {
+    return keys.filter(k => k._id !== keyID);
+  },
+  [UPDATE_BOT_KEYS]: (_, { data }) => { return data; },
+  [ADD_BOT_KEYS]: (keys, { data }) => { return [...keys, data]; },
+};
 
+const receivedKeysReducerList = {
+  [UPDATE_DASHBOARD]: (_, { data }) => { return data.keys.receivedKeys; },
+};
 
-export default combineReducers({ownKeys, receivedKeys, botKeys});
+const ownKeys = reducerCreator([], ownKeysReducerList);
+const botKeys = reducerCreator([], botKeysReducerList);
+const receivedKeys = reducerCreator([], receivedKeysReducerList);
+
+export default combineReducers({ ownKeys, receivedKeys, botKeys });
