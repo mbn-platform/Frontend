0631ca875be786cda8173f73204d1323daf7f6aa
Eugene Zhylenko <jenyajilenko@gmail.com>
2020-02-03 00:09:59 +0200

    refactor for profile reducer

--- src/reducers/profile.js
+++ src/reducers/profile.js
@@ -1,41 +1,34 @@
-import {GET_PROFILE, UPDATE_PROFILE_AVAILABLE, UPDATE_PROFILE, TRADES_FOR_USER, GET_FEEDBACKS, STATS_FOR_USER} from '../actions/profile';
+import { reducerCreator } from 'generic/util';
+import {
+  GET_PROFILE, UPDATE_PROFILE_AVAILABLE, UPDATE_PROFILE,
+  TRADES_FOR_USER, GET_FEEDBACKS, STATS_FOR_USER,
+} from 'actions/profile';
 
-export default function(state = {}, action) {
-  switch(action.type) {
-    case GET_PROFILE:
-      return {...action.profile, trades: [], feedbacks: [], stats: []};
-    case GET_FEEDBACKS:
-      if(action.name === state.name) {
-        return {...state, feedbacks: action.feedbacks};
-      } else {
-        return state;
-      }
-    case UPDATE_PROFILE_AVAILABLE:
-      const {available, contractSettings, currencies} = action.profile;
-      return {...state, available, contractSettings, currencies};
-
-    case UPDATE_PROFILE:
-      return {...state, ...action.profile};
-
-    case TRADES_FOR_USER: {
-      if(action.name === state.name) {
-        const trades = action.trades.filter(ff).map(mf).sort(sf);
-        return {...state, trades};
-      } else {
-        return state;
-      }
-    }
-    case STATS_FOR_USER: {
-      if (action.name === state.name) {
-        return {...state, stats: action.stats, summary: action.summary};
-      } else {
-        return state;
-      }
-    }
-    default:
-      return state;
-  }
-}
+const reducerList = {
+  [GET_PROFILE]: (_, { profile }) => ({
+    ...profile, trades: [], feedbacks: [], stats: [],
+  }),
+  [GET_FEEDBACKS]: (state, { name, feedbacks }) => (
+    name === state.name
+      ? { ...state, feedbacks }
+      : state
+  ),
+  [UPDATE_PROFILE_AVAILABLE]: (state, { profile }) => {
+    const { available, contractSettings, currencies } = profile;
+    return { ...state, available, contractSettings, currencies };
+  },
+  [UPDATE_PROFILE]: (state, { profile }) => ({ ...state, profile }),
+  [TRADES_FOR_USER]: (state, { name, trades }) => (
+    name === state.name
+      ? { ...state, trades: trades.filter(ff).map(mf).sort(sf), }
+      : state
+  ),
+  [STATS_FOR_USER]: (state, { name, stats, summary }) => (
+    name === state.name
+      ? { ...state, stats, summary }
+      : state
+  ),
+};
 
 const mf = t => {
   const [main, secondary] = t.symbol.split('-');
@@ -52,3 +45,5 @@ const mf = t => {
 
 const ff = t => t.filled > 0;
 const sf = (t1, t2) => t2.dt - t1.dt;
+
+export default reducerCreator({}, reducerList);
