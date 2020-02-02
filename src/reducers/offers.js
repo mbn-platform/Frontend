29fb70714f7b929c98afe1d91afe6223f1c3b304
Eugene Zhylenko <jenyajilenko@gmail.com>
2020-02-03 00:08:59 +0200

    refactor for offers reducer

--- src/reducers/offers.js
+++ src/reducers/offers.js
@@ -1,82 +1,58 @@
-import {
-  ACCEPT_OFFER,
-  REJECT_OFFER,
-  CANCEL_OFFER,
-  SEND_OFFER,
-  PAY_OFFER,
-  NEW_OFFER,
-  VERIFY_OFFER,
-  TIMEOUT_OFFER,
-} from '../actions/offers';
-import { FETCH_CONTRACTS } from '../actions/contracts';
 import { combineReducers } from 'redux';
 
-function incoming(state = [], action) {
-  switch(action.type) {
-    case CANCEL_OFFER:
-      if(action.userId !== action.offer.to._id) {
-        return state;
-      }
-      return state.filter(offer => offer._id !== action.offer._id);
-    case REJECT_OFFER:
-      if(action.userId !== action.offer.to._id) {
-        return state;
-      }
-      return state.filter(o => o._id !== action.offer._id);
-    case ACCEPT_OFFER:
-      if(action.userId !== action.offer.to._id) {
-        return state;
-      }
-      return state.map(o => o._id === action.offer._id ? action.offer : o);
-    case FETCH_CONTRACTS:
-      return action.offers.incoming;
-    case NEW_OFFER:
-      const offer = action.offer;
-      return state.concat(offer);
-    case TIMEOUT_OFFER: {
-      const offer = action.offer;
-      return state.filter((o) => o._id !== offer._id);
-    }
-    case VERIFY_OFFER:
-      return state.filter(offer => offer._id !== action.offer._id);
-    default:
-      return state;
-  }
-}
+import { reducerCreator } from 'generic/util';
+import {
+  ACCEPT_OFFER, REJECT_OFFER, CANCEL_OFFER, SEND_OFFER,
+  PAY_OFFER, NEW_OFFER, VERIFY_OFFER, TIMEOUT_OFFER,
+} from 'actions/offers';
+import { FETCH_CONTRACTS } from 'actions/contracts';
 
-function outgoing(state = [], action) {
-  switch(action.type) {
-    case FETCH_CONTRACTS:
-      return action.offers.outgoing;
-    case REJECT_OFFER:
-      if(action.userId !== action.offer.from._id) {
-        return state;
-      }
-      return state.filter(o => o._id !== action.offer._id);
-    case CANCEL_OFFER:
-      if(action.userId !== action.offer.from._id) {
-        return state;
-      }
-      return state.filter(offer => offer._id !== action.offer._id);
-    case ACCEPT_OFFER:
-      if(action.userId !== action.offer.from._id) {
-        return state;
-      }
-      return state.map(o => o._id === action.offer._id ? action.offer : o);
-    case SEND_OFFER:
-      const offer = action.offer;
-      return state.concat(offer);
-    case TIMEOUT_OFFER: {
-      const offer = action.offer;
-      return state.filter((o) => o._id !== offer._id);
-    }
-    case VERIFY_OFFER:
-      return state.filter(offer => offer._id !== action.offer._id);
-    case PAY_OFFER:
-      return state.filter(offer => offer._id !== action.offer._id);
-    default:
-      return state;
-  }
-}
+const incomingReducerList = {
+  [CANCEL_OFFER]: (state, { userId, offer }) => (
+    userId !== offer.to._id
+      ? state
+      : state.filter(o => o._id !== offer._id)
+  ),
+  [REJECT_OFFER]: (state, { userId, offer }) => (
+    userId !== offer.to._id
+      ? state
+      : state.filter(o => o._id !== offer._id)
+  ),
+  [ACCEPT_OFFER]: (state, { userId, offer }) => (
+    userId !== offer.to._id
+      ? state
+      : state.map(o => o._id === offer._id ? offer : o)
+  ),
+  [FETCH_CONTRACTS]: (_, { offers: { incoming } }) => incoming,
+  [NEW_OFFER]: (state, { offer }) => [...state, offer],
+  [TIMEOUT_OFFER]: (state, { offer }) => state.filter(o => o._id !== offer._id),
+  [VERIFY_OFFER]: (state, { offer }) => state.filter(o => o._id !== offer._id),
+};
 
-export default combineReducers({outgoing, incoming});
+const outgoingReducerList = {
+  [FETCH_CONTRACTS]: (_, { offers: { outgoing } }) => outgoing,
+  [REJECT_OFFER]: (state, { userId, offer }) => (
+    userId !== offer.from._id
+      ? state
+      : state.filter(o => o._id !== offer._id)
+  ),
+  [CANCEL_OFFER]: (state, { userId, offer }) => (
+    userId !== offer.from._id
+      ? state
+      : state.filter(o => o._id !== offer._id)
+  ),
+  [ACCEPT_OFFER]: (state, { userId, offer }) => (
+    userId !== offer.from._id
+      ? state
+      : state.map(o => o._id === offer._id ? offer : o)
+  ),
+  [SEND_OFFER]: (state, { offer }) => [...state, offer],
+  [TIMEOUT_OFFER]: (state, { offer }) => state.filter(o => o._id !== offer._id),
+  [VERIFY_OFFER]: (state, { offer }) => state.filter(o => o._id !== offer._id),
+  [PAY_OFFER]: (state, { offer }) => state.filter(o => o._id !== offer._id),
+};
+
+const incoming = reducerCreator([], incomingReducerList);
+const outgoing = reducerCreator([], outgoingReducerList);
+
+export default combineReducers({ incoming, outgoing });
