c2e9bdb29a7675636bf9ce9eb45c4704e5fa0e0d
Eugene Zhylenko <jenyajilenko@gmail.com>
2020-02-04 00:28:18 +0200

    refactor for quickNotif reducer

--- src/reducers/quickNotif.js
+++ src/reducers/quickNotif.js
@@ -1,16 +1,15 @@
-import { ADD_QUICK_NOTIF, REMOVE_QUICK_NOTIF } from '../actions/quickNotif';
+import { reducerCreator } from 'generic/util';
+import { ADD_QUICK_NOTIF, REMOVE_QUICK_NOTIF } from 'actions/quickNotif';
 
-export default function(state = [], action) {
-  switch (action.type) {
-    case ADD_QUICK_NOTIF:
-      if (Array.isArray((action.quickNotif))) {
-        return [...action.quickNotif, ...state];
-      } else {
-        return [action.quickNotif, ...state];
-      }
-    case REMOVE_QUICK_NOTIF:
-      return state.filter((quickNotif) => quickNotif.object._id !== action.quickNotif.object._id);
-    default:
-      return state;
-  }
-}
+const reducerList = {
+  [ADD_QUICK_NOTIF]: (state, { quickNotif }) => (
+    Array.isArray(quickNotif)
+      ? [...quickNotif, ...state]
+      : [quickNotif, ...state]
+  ),
+  [REMOVE_QUICK_NOTIF]: (state, { quickNotif }) => (
+    state.filter(qn => qn.object._id !== quickNotif.object._id)
+  ),
+};
+
+export default reducerCreator([], reducerList);
