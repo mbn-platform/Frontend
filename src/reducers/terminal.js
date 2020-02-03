68e917f5c6be0cf800c1bbab5e33823d0baec8f2
Eugene Zhylenko <jenyajilenko@gmail.com>
2020-02-04 00:30:34 +0200

    refactor for terminal reducer

--- src/reducers/terminal.js
+++ src/reducers/terminal.js
@@ -1,241 +1,209 @@
-import {
-  SELECT_FUND, SELECT_EXCHANGE, SELECT_MARKET, SELECT_INTERVAL, CHECK_URL_VALIDITY,
-  GET_MY_ORDERS, GET_GROUP_ORDER, CANCEL_ORDER, PLACE_ORDER, UPDATE_ORDER,
-  UPDATE_ORDER_BOOK, UPDATE_HISTORY, UPDATE_TICKER, SELECT_ASSET_GROUP,
-} from '../actions/terminal';
-import { UPDATE_ASSET_GROUP, DELETE_ASSET_GROUP } from '../actions/assetGroup';
+import { reducerCreator } from 'generic/util';
+import * as actions from 'actions/terminal';
+import { UPDATE_ASSET_GROUP, DELETE_ASSET_GROUP } from 'actions/assetGroup';
 
-export default function(state = {
+const initialState = {
   fund: null,
   assetGroup: null,
   exchange: 'binance',
   market: 'USDT-BTC',
   interval: '30 MIN',
-  orderBook: {sell: [], buy: [], smap: {}, bmap: {}},
+  orderBook: { sell: [], buy: [], smap: {}, bmap: {} },
   history: [],
   ticker: {},
-  orders: {open: [], closed: []},
+  orders: { open: [], closed: [] },
   isValidUrl: undefined,
-}, action) {
-  switch(action.type) {
-    case SELECT_FUND: {
-      return {...state, fund: action.fund};
-    }
-    case SELECT_MARKET: {
-      if(action.market === state.market) {
-        return state;
-      } else {
-        return {...state, market: action.market, orderBook: {sell: [], buy: [], smap: {}, bmap: {}}, history: [], ticker: null};
-      }
-    }
-    case SELECT_ASSET_GROUP: {
-      return {
-        ...state,
-        assetGroup: action.group,
-        orders: action.group ? state.orders : { open: [], closed: [] },
-        fund: null,
-      };
-    }
-    case DELETE_ASSET_GROUP: {
-      return {
-        ...state,
-        assetGroup: null,
-        orders: state.assetGroup ? { open: [], closed: [] } : state.orders,
-      };
-    }
-    case UPDATE_ASSET_GROUP: {
-      const { assetGroup } = state;
+};
 
-      return { ...state, assetGroup: assetGroup ? action.assetGroup : null };
-    }
-    case SELECT_EXCHANGE: {
-      if(action.exchange === state.exchange) {
-        return state;
-      } else {
-        return {...state, exchange: action.exchange, orderBook: {sell: [], buy: [], smap: {}, bmap: {}}, history: []};
-      }
-    }
-    case SELECT_INTERVAL: {
-      if(action.interval === state.interval) {
-        return state;
-      } else {
-        return {...state, interval: action.interval};
-      }
-    }
-    case UPDATE_ORDER_BOOK: {
-      if(!(action.market === state.market && action.exchange === state.exchange)) {
-        return state;
-      }
-      let orderBook = {...state.orderBook};
-      if (action.isFull) {
-        orderBook = {sell: [], buy: [], smap: {}, bmap: {}};
-      }
-      if (action.orderBook.sell.length) {
-        const smap = {...orderBook.smap};
-        for(const o of action.orderBook.sell) {
-          const [value, amount] = o;
-          if(amount === 0) {
-            delete smap[value];
-          } else {
-            smap[value] = {Quantity: amount, Rate: value};
-          }
-        }
-        const sell = Object.values(smap).sort((o1, o2) => o1.Rate - o2.Rate);
-        const maxSell = sell.reduce((accum, value) => Math.max(accum, value.Quantity * value.Rate), 0);
-        const minSell = sell.reduce((accum, value) => Math.min(accum, value.Quantity * value.Rate), maxSell);
-        orderBook.smap = smap;
-        orderBook.sell = sell;
-        orderBook.maxSell = maxSell;
-        orderBook.minSell = minSell;
-      }
-      if (action.orderBook.buy.length) {
-        const bmap = {...orderBook.bmap};
-        for(const o of action.orderBook.buy) {
-          const [value, amount] = o;
-          if(amount === 0) {
-            delete bmap[value];
-          } else {
-            bmap[value] = {Quantity: amount, Rate: value};
-          }
-        }
-        const buy = Object.values(bmap).sort((o1, o2) => o2.Rate - o1.Rate);
-        const maxBuy = buy.reduce((accum, value) => Math.max(accum, value.Quantity * value.Rate), 0);
-        const minBuy = buy.reduce((accum, value) => Math.min(accum, value.Quantity * value.Rate), maxBuy);
-        orderBook.bmap = bmap;
-        orderBook.buy = buy;
-        orderBook.maxBuy = maxBuy;
-        orderBook.minBuy = minBuy;
-      }
-      return {...state, orderBook };
-    }
-    case UPDATE_HISTORY: {
-      if((action.market !== state.market && action.exchange !== state.exchange)) {
-        return state;
-      }
+const reducerList = {
+  [actions.SELECT_FUND]: (state, { fund }) => ({ ...state, fund }),
+  [actions.SELECT_MARKET]: (state, { market }) => (
+    market === state.market
+      ? state
+      : { ...state, market, orderBook: initialState.orderBook, history: [], ticker: null }
+  ),
+  [actions.SELECT_ASSET_GROUP]: (state, { group }) => ({
+    ...state,
+    assetGroup: group,
+    orders: group ? state.orders : initialState.orders,
+    fund: null,
+  }),
+  [DELETE_ASSET_GROUP]: (state) => ({
+    ...state,
+    assetGroup: null,
+    orders: state.assetGroup ? initialState.orders : state.orders,
+  }),
+  [UPDATE_ASSET_GROUP]: (state, { assetGroup }) => ({ ...state, assetGroup: assetGroup || null }),
+  [actions.SELECT_EXCHANGE]: (state, { exchange }) => (
+    exchange === state.exchange
+      ? state
+      : { ...state, exchange, orderBook: initialState.orderBook, history: [] }
+  ),
+  [actions.SELECT_INTERVAL]: (state, { interval }) => (
+    interval === state.exchange ? state : { ...state, interval }
+  ),
+  [actions.UPDATE_ORDER_BOOK]: (state, { market, exchange, orderBook }) => {
+    if (!(market === state.market && exchange === state.exchange)) { return state; }
 
-      const history = action.history
-        .sort((t1, t2) => t2[2] - t1[2])
-        .map(t => ({
-          id: Math.random().toFixed(8),
-          price: t[0],
-          amount: t[1],
-          type: t[3],
-          dt: t[2],
-        }));
-
-      return {...state, history: history.concat(state.history).slice(0, 50)};
-    }
-    case UPDATE_TICKER: {
-      if(action.exchange === state.exchange && action.market === state.market) {
-        return {...state, ticker: action.ticker};
-      }
-      return state;
-    }
-    case GET_MY_ORDERS: {
-      const fund = state.fund || state.assetGroup;
-      if(fund && fund._id === action.fundId) {
-        return {...state, orders: action.orders};
-      }
-      break;
-    }
-
-    case GET_GROUP_ORDER: {
-      const { open, closed } = state.orders;
-      const order = open.find(order => order._id === action.order._id);
-
-      if (order) {
-        return {
-          ...state,
-          orders: {
-            open: open.map(item => item._id === order._id ? action.order : item),
-            closed,
-          },
-        };
-      } else {
-        return {
-          ...state,
-          orders: {
-            open,
-            closed: closed.map(item => item._id === action.order._id ? action.order : item),
-          },
-        };
-      }
-    }
-
-    case PLACE_ORDER: {
-      const order = state.orders.open.find(o => o._id === action.order._id);
-      if(!order) {
-        let closed = state.orders.closed;
-        let opened = state.orders.open;
-        if (action.order.state === 'CLOSED') {
-          closed = [action.order, ...closed];
+    const newOrderBook = {...state.orderBook};
+    if (orderBook.sell.length) {
+      const newSmap = { ...state.orderBook.smap };
+      for (const o of orderBook.sell) {
+        const [value, amount] = o;
+        if (amount === 0) {
+          delete newSmap[value];
         } else {
-          opened = [action.order, ...opened];
+          newSmap[value] = { Quantity: amount, Rate: value };
         }
-        const orders = {
-          open: opened,
-          closed: closed,
-        };
-        return {...state, orders};
-      } else {
-        return state;
       }
-    }
-    case UPDATE_ORDER: {
-      if (!(state.fund && state.fund._id === action.fundId)) {
-        return state;
-      }
-      let { open, closed } = state.orders;
-      const order = action.order;
-      switch (order.state) {
-        case 'OPEN': {
-          const old = open.find((o) => o._id === order._id);
-          if (old) {
-            open = open.map((o) => o._id === order._id ? order : o);
-          } else {
-            open = [order, ...open];
-          }
-          break;
-        }
-        case 'CLOSED': {
-          const old = closed.find((o) => o._id === order._id);
-          const oldOpen = open.find((o) => o._id === order._id);
-          if (old) {
-            closed = closed.map((o) => o._id === order._id ? order: o);
-          } else if (!order.isAlgo) {
-            closed = [order, ...closed];
-          }
-          if (oldOpen) {
-            open = open.filter((o) => o._id !== order._id);
-          }
-          break;
-        }
-        default:
-          return state;
-      }
-      return {...state, orders: { open, closed }};
-    }
-    case CANCEL_ORDER: {
-      const order = state.orders.open.find(o => o._id === action.order._id);
-      if(order) {
-        const orders = {
-          open: state.orders.open.filter(o => o._id !== action.order._id),
-          closed: state.orders.closed,
-        };
-        if(order.filled > 0) {
-          orders.closed = [action.order, ...state.orders.closed];
-        }
-        return {...state, orders};
-      }
-      break;
+      const sell = Object.values(newSmap).sort((o1, o2) => o1.Rate - o2.Rate);
+      const maxSell = sell.reduce((accum, value) => Math.max(accum, value.Quantity * value.Rate), 0);
+      const minSell = sell.reduce((accum, value) => Math.min(accum, value.Quantity * value.Rate), maxSell);
+      newOrderBook.smap = newSmap;
+      newOrderBook.sell = sell;
+      newOrderBook.maxSell = maxSell;
+      newOrderBook.minSell = minSell;
     }
 
-    case CHECK_URL_VALIDITY: {
-      const { isValidUrl } = action;
-      return { ...state, isValidUrl };
+    if (orderBook.buy.length) {
+      const newBmap = { ...state.orderBook.bmap };
+      for(const o of orderBook.buy) {
+        const [value, amount] = o;
+        if(amount === 0) {
+          delete newBmap[value];
+        } else {
+          newBmap[value] = {Quantity: amount, Rate: value};
+        }
+      }
+      const buy = Object.values(newBmap).sort((o1, o2) => o2.Rate - o1.Rate);
+      const maxBuy = buy.reduce((accum, value) => Math.max(accum, value.Quantity * value.Rate), 0);
+      const minBuy = buy.reduce((accum, value) => Math.min(accum, value.Quantity * value.Rate), maxBuy);
+      newOrderBook.bmap = newBmap;
+      newOrderBook.buy = buy;
+      newOrderBook.maxBuy = maxBuy;
+      newOrderBook.minBuy = minBuy;
     }
-    default:
+
+    return { ...state, orderBook: newOrderBook };
+  },
+  [actions.UPDATE_HISTORY]: (state, { market, exchange, history }) => {
+    if ((market !== state.market && exchange !== state.exchange)) { return state; }
+
+    const newHistory = history
+      .sort((t1, t2) => t2[2] - t1[2])
+      .map(t => ({
+        id: Math.random().toFixed(8),
+        price: t[0],
+        amount: t[1],
+        type: t[3],
+        dt: t[2],
+      }));
+
+    return { ...state, history: newHistory.concat(state.history).slice(0, 50) };
+  },
+  [actions.UPDATE_TICKER]: (state, { exchange, market, ticker }) => (
+    exchange === state.exchange && market === state.market
+      ? { ...state, ticker }
+      : state
+  ),
+  [actions.GET_MY_ORDERS]: (state, { orders, fundId }) => {
+    const fund = state.fund || state.assetGroup;
+    if (fund && fund._id === fundId) {
+      return { ...state, orders };
+    }
+  },
+  [actions.GET_GROUP_ORDER]: (state, action) => {
+    const { open, closed } = state.orders;
+    const order = open.find(order => order._id === action.order._id);
+
+    if (order) {
+      return {
+        ...state,
+        orders: {
+          open: open.map(item => item._id === order._id ? action.order : item),
+          closed,
+        },
+      };
+    } else {
+      return {
+        ...state,
+        orders: {
+          open,
+          closed: closed.map(item => item._id === action.order._id ? action.order : item),
+        },
+      };
+    }
+  },
+  [actions.PLACE_ORDER]: (state, action) => {
+    const order = state.orders.open.find(o => o._id === action.order._id);
+
+    if (!order) {
+      let closed = state.orders.closed;
+      let opened = state.orders.open;
+      if (action.order.state === 'CLOSED') {
+        closed = [action.order, ...closed];
+      } else {
+        opened = [action.order, ...opened];
+      }
+      const orders = {
+        open: opened,
+        closed: closed,
+      };
+      return { ...state, orders };
+    } else {
       return state;
-  }
-  return state;
-}
+    }
+  },
+  [actions.UPDATE_ORDER]: (state, action) => {
+    if (!(state.fund && state.fund._id === action.fundId)) { return state; }
+
+    let { open, closed } = state.orders;
+    const order = action.order;
+    switch (order.state) {
+      case 'OPEN': {
+        const old = open.find((o) => o._id === order._id);
+        if (old) {
+          open = open.map((o) => o._id === order._id ? order : o);
+        } else {
+          open = [order, ...open];
+        }
+        break;
+      }
+      case 'CLOSED': {
+        const old = closed.find((o) => o._id === order._id);
+        const oldOpen = open.find((o) => o._id === order._id);
+        if (old) {
+          closed = closed.map((o) => o._id === order._id ? order: o);
+        } else if (!order.isAlgo) {
+          closed = [order, ...closed];
+        }
+        if (oldOpen) {
+          open = open.filter((o) => o._id !== order._id);
+        }
+        break;
+      }
+      default:
+        return state;
+    }
+
+    return { ...state, orders: { open, closed } };
+  },
+  [actions.CANCEL_ORDER]: (state, action) => {
+    const order = state.orders.open.find(o => o._id === action.order._id);
+
+    if (order) {
+      const orders = {
+        open: state.orders.open.filter(o => o._id !== action.order._id),
+        closed: state.orders.closed,
+      };
+
+      if (order.filled > 0) {
+        orders.closed = [action.order, ...state.orders.closed];
+      }
+      return { ...state, orders };
+    }
+  },
+  [actions.CHECK_URL_VALIDITY]: (state, { isValidUrl }) => ({ ...state, isValidUrl }),
+};
+
+export default reducerCreator(initialState, reducerList);
