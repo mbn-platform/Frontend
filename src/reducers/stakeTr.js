7f2be3e9ff28a1cef24519a0b30508d23bb99fad
Eugene Zhylenko <jenyajilenko@gmail.com>
2020-02-04 00:26:08 +0200

    refactor for stakeTr reducer

--- src/reducers/stakeTr.js
+++ src/reducers/stakeTr.js
@@ -1,10 +1,18 @@
-import { GET_TR_BLOCK , SET_TR_PAGE_SIZE, SET_TR_PAGE } from '../actions/profile';
-import { listReducer } from './util';
+import { reducerCreator } from 'generic/util';
+import { GET_TR_BLOCK , SET_TR_PAGE_SIZE, SET_TR_PAGE } from 'actions/profile';
 
-export default listReducer({
-  get: GET_TR_BLOCK,
-  setPageSize: SET_TR_PAGE_SIZE,
-  setPage: SET_TR_PAGE,
-}, null, {
+export const initialState = {
+  list: [],
+  page: 1,
   pageSize: 10,
-});
+  count: 0,
+};
+
+const reducerList = {
+  [GET_TR_BLOCK]: (state, { list, count }) => ({ ...state, list, count }),
+  [SET_TR_PAGE_SIZE]: (state, { pageSize }) => ({ ...state, pageSize }),
+  [SET_TR_PAGE]: (state, { page }) => ({ ...state, page }),
+};
+
+export default reducerCreator(initialState, reducerList);
+
