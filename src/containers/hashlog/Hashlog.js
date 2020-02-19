d22f0af526d4cf3418c3d29167cbe1a554b49285
Eugene Zhylenko <jenyajilenko@gmail.com>
2020-02-19 23:22:38 +0200

    refactor hashlog components with selectors

--- src/containers/hashlog/Hashlog.js
+++ src/containers/hashlog/Hashlog.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
 import { FormattedMessage, FormattedDate, injectIntl } from 'react-intl';
-import { getBlockListPage, setBlockListPage, setBlockListPageSize } from '../../actions/hashlog';
-import { setBlockForActionList } from '../../actions/actionsList';
-import ReactTable from '../../components/SelectableReactTable';
-import createMqProvider, {querySchema} from '../../MediaQuery';
-import PaginationWithPage from '../../components/PaginationWithPage';
 import qs from 'qs';
+import { compose } from 'ramda';
 
-const {Screen} = createMqProvider(querySchema);
+import createMqProvider, {querySchema} from 'MediaQuery';
+import { setBlockForActionList } from 'actions/actionsList';
+import ReactTable from 'components/SelectableReactTable';
+import PaginationWithPage from 'components/PaginationWithPage';
+import { getBlockListPage, setBlockListPage, setBlockListPageSize } from 'actions/hashlog';
+import { actionListSelector } from 'selectors/actionList';
+import { hashlogSelector } from 'selectors/hashlog';
+
+const { Screen } = createMqProvider(querySchema);
 
 class Hashlog extends React.Component {
   state = {
@@ -20,29 +24,26 @@ class Hashlog extends React.Component {
   };
 
   componentDidMount() {
-    const {getBlocksPages, hashlog : {blocksPage, blocksPageSize}} = this.props;
+    const { getBlocksPages, hashlog : { blocksPage, blocksPageSize } } = this.props;
     getBlocksPages(blocksPage, blocksPageSize);
   }
 
-  onRowClick = rowData=> {
-    const { 
-      setActionBlock,
-      history,
-    } = this.props;
-    const requestParams= qs.stringify({
-      blockNumber: rowData.original.number,
-    });
-    history.push('/hashlog/actions/?' + requestParams);
+  onRowClick = rowData => {
+    const { setActionBlock, history } = this.props;
+
+    const requestParams= qs.stringify({ blockNumber: rowData.original.number });
+    history.push(`/hashlog/actions/?${requestParams}`);
     setActionBlock(rowData.original);
   }
 
-  renderBlocklist = () => {
+  renderBlockList = () => {
     const {
-      hashlog: {blockList, totalBlocks, blocksPage, blocksPageSize},
+      hashlog: { blockList, totalBlocks, blocksPage, blocksPageSize },
       setBlocksPage,
       setBlocksPageSize,
       getBlocksPages,
     } = this.props;
+
     return (
       <React.Fragment>
         <Screen on={screenWidth => (
@@ -140,41 +141,41 @@ class Hashlog extends React.Component {
     ];
   };
 
-  render() {
-    return (
-      <Container fluid className="ratings">
-        <Row>
-          <Col xs="12" sm="12" md="12" lg="12">
-            <div className="hashlog__main">
-              <div className="hashlog__main-title">
-                <FormattedMessage
-                  id="hashlog.title"
-                  defaultMessage="Hashlog"
-                />
-              </div>
-              <div className="hashlog__main-board">
-                {this.renderBlocklist()}
-              </div>
+  render = () => (
+    <Container fluid className="ratings">
+      <Row>
+        <Col xs="12" sm="12" md="12" lg="12">
+          <div className="hashlog__main">
+            <div className="hashlog__main-title">
+              <FormattedMessage
+                id="hashlog.title"
+                defaultMessage="Hashlog"
+              />
             </div>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+            <div className="hashlog__main-board">
+              {this.renderBlockList()}
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 const mapStateToProps = state => ({
-  actionList: state.actionList,
-  hashlog: state.hashlog,
+  actionList: actionListSelector(state),
+  hashlog: hashlogSelector(state),
 });
 
-const mapDispatchToProps = dispatch => {
-  return {
-    getBlocksPages: (pages, size) => dispatch(getBlockListPage(pages, size)),
-    setActionBlock: blockInfo => dispatch(setBlockForActionList(blockInfo)),
-    setBlocksPage: page => dispatch(setBlockListPage(page)),
-    setBlocksPageSize: pageSize => dispatch(setBlockListPageSize(pageSize)),
-  };
+const mapDispatchToProps = {
+  getBlocksPages: getBlockListPage,
+  setActionBlock: setBlockForActionList,
+  setBlocksPage: setBlockListPage,
+  setBlocksPageSize: setBlockListPageSize,
 };
 
-export default withRouter(injectIntl(connect(mapStateToProps, mapDispatchToProps)(Hashlog)));
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  injectIntl,
+  withRouter,
+)(Hashlog);
