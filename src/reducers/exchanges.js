8ef2496b64137ceda46ef161a00e3b262e8c36c4
Eugene Zhylenko <jenyajilenko@gmail.com>
2020-02-25 23:04:39 +0200

    remove hardcoded code

--- src/reducers/exchanges.js
+++ src/reducers/exchanges.js
@@ -3,8 +3,7 @@ import { UPDATE_EXCHANGES } from '../actions/exchanges';
 export default function(state = [], action) {
   switch(action.type) {
     case UPDATE_EXCHANGES:
-      // TODO: remove when will be implemented on backend side
-      return action.exchanges.concat('kucoin');
+      return action.exchanges;
     default:
       return state;
   }
