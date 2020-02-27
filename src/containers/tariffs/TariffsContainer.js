bb07e9418dfb51a0f055197a0ed74e1e325af1ad
Eugene Zhylenko <jenyajilenko@gmail.com>
2020-02-27 22:17:11 +0200

    refactor tariffs components with selectors

--- src/containers/tariffs/TariffsContainer.js
+++ src/containers/tariffs/TariffsContainer.js
@@ -1,23 +1,17 @@
 import { connect } from 'react-redux';
 
-import { fetchTariffs, createPaymentRequest } from '../../actions/tariffs';
+import { fetchTariffs, createPaymentRequest } from 'actions/tariffs';
 import Tariffs from './Tariffs';
+import { tariffsSelector } from 'selectors/tariffs';
+import { loggedInSelector, billingSelector } from 'selectors/auth';
+import { paymentRequestSelector } from 'selectors/payments';
 
-const mapStateToProps = ({ auth, tariffs, payments: { paymentRequest }}) => {
-  let billing;
-  const loggedIn = auth.loggedIn;
-
-  if (loggedIn) {
-    billing = auth.profile.billing;
-  }
-
-  return {
-    loggedIn,
-    billing,
-    tariffs,
-    paymentRequest,
-  };
-};
+const mapStateToProps = (state) => ({
+  tariffs: tariffsSelector(state),
+  loggedIn: loggedInSelector(state),
+  paymentRequest: paymentRequestSelector(state),
+  billing: billingSelector(state),
+});
 
 const mapDispatchToProps = {
   fetchTariffs,
---
+++ src/selectors/tariffs.js
@@ -0,0 +1,3 @@
+import * as R from 'ramda';
+
+export const tariffsSelector = R.prop('tariffs');
